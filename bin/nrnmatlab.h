8e95b376fc94974052eeb0386cb6731b6eaf083a
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2022-09-16 13:26:05 +0200

    Add linux compatibility to setup.m and header

--- bin/nrnmatlab.h
+++ bin/nrnmatlab.h
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 
+#ifdef _WIN32
 // Import C++ name mangled functions.
 __declspec(dllimport) void ivocmain_session(int, const char**, 
                                             const char**, int);
-
 // Import non-name mangled functions.
 extern "C" __declspec(dllimport) int hoc_oc(const char*);
 extern "C" __declspec(dllimport) void* hoc_lookup(const char*);
 extern "C" __declspec(dllimport) double hoc_call_func(void*, int);
 extern "C" __declspec(dllimport) void hoc_pushx(double);
+#else 
+// Import non-name mangled functions.
+extern "C" void ivocmain_session(int, const char**, const char**, int);
+extern "C" int hoc_oc(const char*);
+extern "C" void* hoc_lookup(const char*);
+extern "C" double hoc_call_func(void*, int);
+extern "C" void hoc_pushx(double);
+#endif
 
 // Define invocmain_session input.
 static const char* argv[] = {"nrn_test", "-nogui", NULL};
