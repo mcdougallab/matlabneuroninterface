c0ecf6d8e6e2cf117a0885ce9ad16ab7dc48586a
edovanveen <edovanveen@gmail.com>
2022-11-11 10:29:31 +0100

    Updated comments: example.crash.m no longer crashes

--- examples/example_crash.m
+++ examples/example_crash.m
@@ -1,4 +1,4 @@
-% Cause a crash while running NEURON in MATLAB:
+% Try to cause a crash while running NEURON in MATLAB:
 clear;
 setup;
 n = neuron.Neuron();
@@ -6,10 +6,4 @@ n.reset_sections();
 v = n.Vector(10);
 
 disp(v.contains());  
-% Prints stderr to screen before crashing:
-%
-% nrn_test: contains not enough arguments
-%  near line 0
-%  objref hoc_obj_[2]
-%                    ^
-%         Vector[0].contains()
+% With NEURON 9, this prints error to screen and does not crash!
