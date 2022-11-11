7d6b0b4d304b6ea5eeed4b4bec867620ac1c9f08
edovanveen <edovanveen@gmail.com>
2022-11-11 11:25:30 +0100

    Testing nrn_try_catch_nest_depth

--- source/nrnmatlab.cpp
+++ source/nrnmatlab.cpp
@@ -57,6 +57,12 @@ bool isinitialized() {
     return initialized;
 }
 
+// Increase try/catch nest depth after catching an error.
+int increase_try_catch_nest_depth() {
+    nrn_try_catch_nest_depth++;
+    return nrn_try_catch_nest_depth;
+}
+
 // Return all functions/methods/attributes as a string with separators ";"
 // between methods, and ":" between method name and method type.
 std::string str_symbol_table(Symlist* table) {
