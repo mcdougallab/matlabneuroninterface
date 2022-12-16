2a43222338357dbbe4b3561582806739a6650f68
edovanveen <edo.vanveen@vortech.nl>
2022-12-16 10:35:07 +0100

    Fixed crash for Vector.contain()

--- source/neuron_dllimports.h
+++ source/neuron_dllimports.h
@@ -8,6 +8,7 @@ __declspec(dllimport) vv_function delete_section;
 __declspec(dllimport) initer_function ivocmain_session;
 __declspec(dllimport) vsecptri_function mech_insert1;
 __declspec(dllimport) voptrsptritemptrptri_function new_sections;
+__declspec(dllimport) nptrsecptrd_function node_exact;
 __declspec(dllimport) vv_function nrnmpi_stubs;
 __declspec(dllimport) ppoptr_function ob2pntproc_0;
 __declspec(dllimport) cptrsecptr_function secname;
@@ -49,6 +50,12 @@ __declspec(dllimport) Objectdata* hoc_top_level_data;
 __declspec(dllimport) Symlist* hoc_top_level_symlist;
 __declspec(dllimport) dv_function hoc_xpop;
 
+// C++ name mangled oc_* functions.
+__declspec(dllimport) hoc_oop_ss oc_save_hoc_oop;
+__declspec(dllimport) hoc_oop_ss oc_restore_hoc_oop;
+__declspec(dllimport) cabcode_ss oc_save_cabcode;
+__declspec(dllimport) cabcode_ss oc_restore_cabcode;
+
 // Import non-name mangled functions and parameters.
 extern "C" __declspec(dllimport) int diam_changed;
 extern "C" __declspec(dllimport) int nrn_is_python_extension;
@@ -56,6 +63,16 @@ extern "C" __declspec(dllimport) int nrn_main_launch;
 extern "C" __declspec(dllimport) int nrn_nobanner_;
 extern "C" __declspec(dllimport) int nrn_try_catch_nest_depth;
 extern "C" __declspec(dllimport) vf2icif_function nrnpy_set_pr_etal;
-extern "C" __declspec(dllimport) nptrsecptrd_function node_exact;
+
+// Import functions for which name mangling goes awry.
+extern "C" __declspec(dllimport) code_ss _Z12oc_save_codePP4InstS1_RyPPN3nrn2oc5frameEPiS8_S1_S7_S2_PP7SymlistS1_S8_;
+const auto oc_save_code = _Z12oc_save_codePP4InstS1_RyPPN3nrn2oc5frameEPiS8_S1_S7_S2_PP7SymlistS1_S8_;
+extern "C" __declspec(dllimport) code_ss _Z15oc_restore_codePP4InstS1_RyPPN3nrn2oc5frameEPiS8_S1_S7_S2_PP7SymlistS1_S8_;
+const auto oc_restore_code = _Z15oc_restore_codePP4InstS1_RyPPN3nrn2oc5frameEPiS8_S1_S7_S2_PP7SymlistS1_S8_;
+extern "C" __declspec(dllimport) input_info_ss _Z18oc_save_input_infoPPKcPiS2_PP6_iobuf;
+const auto oc_save_input_info = _Z18oc_save_input_infoPPKcPiS2_PP6_iobuf;
+extern "C" __declspec(dllimport) input_info_rs _Z21oc_restore_input_infoPKciiP6_iobuf;
+const auto oc_restore_input_info = _Z21oc_restore_input_infoPKciiP6_iobuf;
+
 
 #endif
