26dcb589f1aac121b060765e16a13dbe54f45190
edovanveen <edo.vanveen@vortech.nl>
2023-01-02 13:49:29 +0100

    Fixes after rebase

--- setup.m
+++ setup.m
@@ -3,15 +3,15 @@
 function setup()
 
     % User setting:
-    NeuronInstallationDirectory = 'C:\nrn-dev';
+    NeuronInstallationDirectory  = 'C:\nrn-dev';
 
     % Check if NEURON directory is correct.
-    filename = fullfile(nrnpath, 'bin', 'libnrniv.dll');
+    filename = fullfile(NeuronInstallationDirectory , 'bin', 'libnrniv.dll');
     assert(exist(filename, 'file') == 2, 'NEURON directory not found.');
     
     % All dependencies of the generated interface library must be findable.
     % WINDOWS: Put them on the PATH
-    dllpath = fullfile(nrnpath, 'bin');
+    dllpath = fullfile(NeuronInstallationDirectory , 'bin');
     syspath = getenv('PATH'); 
     if ~contains(string(syspath), string(dllpath)+pathsep)
         setenv('PATH', [dllpath pathsep syspath]);
