e4230a4d07ca32e165938bdaec976b3b3a835012
edovanveen <edo.vanveen@vortech.nl>
2023-04-14 14:25:14 +0200

    Update example_rangevarplot.m

--- examples/example_rangevarplot.m
+++ examples/example_rangevarplot.m
@@ -1,7 +1,22 @@
+% Init neuron.
 clear;
 setup;
 n = neuron.Neuron();
 n.reset_sections();
 
-axon = n.Section("axon");
-rvp = n.RangeVarPlot(axon, "v");
+% Make section with varying voltage.
+dend = n.Section("dend");
+dend.nseg = 55;
+dend.length = 6.28;
+for i=0:dend.nseg+1
+    segment = double(i) / double(dend.nseg+1);
+    v_ref = dend.ref("v", segment);
+    disp(segment * dend.length);
+    v_ref.set(sin(segment * dend.length));
+end
+
+% Plot result with RangeVarPlot.
+rvp = n.RangeVarPlot(dend, "v", 0, 1);
+v = n.Vector();
+rvp.to_vector(v);
+plot(v);
