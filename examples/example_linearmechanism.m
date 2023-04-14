11c6680d6ead889b5cc8692ed52f74c89de12227
edovanveen <edo.vanveen@vortech.nl>
2023-04-14 16:48:05 +0200

    Compare linmech to hoc file output: same result

--- examples/example_linearmechanism.m
+++ examples/example_linearmechanism.m
@@ -6,6 +6,7 @@ n.reset_sections();
 
 % Create section.
 soma = n.Section("soma");
+soma.insert_mechanism("hh");
 
 % Test LinearMechanism
 c = n.Matrix(2, 2, 2);
@@ -15,10 +16,17 @@ b = n.Vector(2);
 g.setval(0, 1, -1);
 g.setval(1, 0, 1);
 b.set(1, 10);
-linmech = n.LinearMechanism(c, g, y, b, 0.5);
+linmech = n.LinearMechanism(soma, c, g, y, b, 0.5);
+
+% Run simulation.
+n.finitialize(-65);
+v_ref = soma.ref("v", 0.5);
 n.t = 0;
 disp(n.t + " " + y.double(1) + " " + y.double(2));
-for i=1:3
+for i=1:20
     n.fadvance();
-    disp(n.t + " " + y.double(1) + " " + y.double(2));
+    disp(n.t + " " + v_ref.get() + " " + y.double(2));
 end
+
+% Compare.
+n.load_file("examples/test_lm.hoc");
