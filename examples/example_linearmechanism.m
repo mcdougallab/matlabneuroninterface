074b6b5928de9b9b543851b844cfc4869e4ab87a
edovanveen <edo.vanveen@vortech.nl>
2023-04-14 16:06:23 +0200

    Update examples.

--- examples/example_linearmechanism.m
+++ examples/example_linearmechanism.m
@@ -7,6 +7,7 @@ n.reset_sections();
 % Create section.
 soma = n.Section("soma");
 
+% Test LinearMechanism
 c = n.Matrix(2, 2, 2);
 g = n.Matrix(2, 2);
 y = n.Vector(2);
@@ -15,3 +16,9 @@ g.setval(0, 1, -1);
 g.setval(1, 0, 1);
 b.set(1, 10);
 linmech = n.LinearMechanism(c, g, y, b, 0.5);
+n.t = 0;
+disp(n.t + " " + y.double(1) + " " + y.double(2));
+for i=1:3
+    n.fadvance();
+    disp(n.t + " " + y.double(1) + " " + y.double(2));
+end
--- examples/example_rangevarplot.m
+++ examples/example_rangevarplot.m
@@ -4,19 +4,26 @@ setup;
 n = neuron.Neuron();
 n.reset_sections();
 
-% Make section with varying voltage.
+% Make section.
 dend = n.Section("dend");
-dend.nseg = 55;
+dend.nseg = 100;
 dend.length = 6.28;
-for i=0:dend.nseg+1
-    segment = double(i) / double(dend.nseg+1);
+
+% Set voltage; also set explicitly at start and end points.
+v_ref = dend.ref("v", 0);
+v_ref.set(sin(0 * dend.length))
+for i=0:dend.nseg-1
+    segment = (double(i) + 0.5) / double(dend.nseg);
     v_ref = dend.ref("v", segment);
     disp(segment * dend.length);
     v_ref.set(sin(segment * dend.length));
 end
+v_ref = dend.ref("v", 1);
+v_ref.set(sin(1 * dend.length))
 
 % Plot result with RangeVarPlot.
 rvp = n.RangeVarPlot(dend, "v", 0, 1);
-v = n.Vector();
-rvp.to_vector(v);
-plot(v);
+x = n.Vector();
+y = n.Vector();
+rvp.to_vector(y, x);
+plot(x, y);
