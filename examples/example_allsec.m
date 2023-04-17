d96ec852a37cf9927b8e248e8aee5e922f950b6c
edovanveen <edo.vanveen@vortech.nl>
2023-04-17 20:53:01 +0200

    Expand allsec example.

--- examples/example_allsec.m
+++ examples/example_allsec.m
@@ -10,11 +10,26 @@ n.reset_sections();
 % Make sections.
 n.hoc_oc("create soma");
 dend1 = n.Section("dend1");
-n.hoc_oc("create axon");
+dend1.length = 37;
+clear dend1;  % Remove MATLAB object from workspace; still exists in NEURON.
+n.hoc_oc("create axon1");
+dend2 = n.Section("dend2");
+delete(dend2);  % Remove NEURON C++ object.
+clear dend2;  % Remove MATLAB object from workspace.
+axon2 = n.Section("axon2");
+axon2.length = 42;
 n.topology();
 
 % Iterate over sections.
 all_sections = neuron.allsec();
 for i=1:width(all_sections)
-    disp(i + " " + clib.neuron.secname(all_sections{i}));
+    disp(i + " " + all_sections{i}.name);
 end
+
+% Get all Sections back in the MATLAB workspace.
+soma = all_sections{1};
+dend1 = all_sections{2};
+axon1 = all_sections{3};
+axon2 = all_sections{4};
+disp("dend1 length: " + dend1.length);
+disp("axon2 length: " + axon2.length);
