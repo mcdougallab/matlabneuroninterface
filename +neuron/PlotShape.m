d4a988bdee64bb12c49350935187195a42229bd8
edovanveen <edo.vanveen@vortech.nl>
2023-04-21 14:04:12 +0200

    Interpolate pt3d points.

--- +neuron/PlotShape.m
+++ +neuron/PlotShape.m
@@ -20,9 +20,14 @@ classdef PlotShape < neuron.Object
             % - Make a 3D plot of spi.varname() at each Segment  
             %   location, with lower bound spi_some.low() and upper 
             %   bound spi_some.high(); also include all 3D points.
+
+            % Call n.define_shape() first? 
+            % sym = clib.neuron.hoc_lookup("define_shape");
+            % clib.neuron.hoc_call_func(sym, 0);
+
             spi = clib.neuron.get_plotshape_interface(self.obj);
             disp("PlotShape, plotting " + spi.varname() + " from " + ...
-                 spi.low() + " to " + spi.high())
+                 spi.low() + " to " + spi.high());
             sl = spi.neuron_section_list();
             secs = neuron.allsec(sl);
             for i=1:numel(secs)
@@ -30,20 +35,29 @@ classdef PlotShape < neuron.Object
                 disp("- Section " + s.name);
 
                 % Get all 3D point information.
-                npt3d = s.get_sec().npt3d;
-                for j=1:npt3d
-                    pt = s.get_sec().pt3d(j);
-                    disp("  - pt3d {x: " + pt.x + ", y: " + pt.y + ...
-                         ", z: " + pt.z + ", d: " + pt.d + "}");
+                pt3d = s.get_pt3d();
+                for j=1:s.get_sec().npt3d
+                    disp("  - pt3d [x, y, z, arc, d]: " + ...
+                         pt3d(1, j) + ", " + ...
+                         pt3d(2, j) + ", " + ...
+                         pt3d(3, j) + ", " + ...
+                         pt3d(4, j) + ", " + ...
+                         pt3d(5, j) + "]");
                 end
 
-                % Get all segment information.
-                segments = s.segments();
-                for j=1:s.nseg
+                % Interpolate to get segment 3d point.
+                segments = s.segments(true);
+                for j=1:numel(segments)
                     seg = segments{j};
+                    seg_l = seg.x * s.length;
+                    seg_x3d = interp1(pt3d(4, :), pt3d(1, :), seg_l);
+                    seg_y3d = interp1(pt3d(4, :), pt3d(2, :), seg_l);
+                    seg_z3d = interp1(pt3d(4, :), pt3d(3, :), seg_l);
+
                     var = spi.varname();
-                    disp("  - segment x: " + seg.x + ...
-                         ", " + var + ": " + s.ref(var, seg.x).get());
+                    disp("  - segment x: " + seg.x + ", " + ...
+                         var + ": " + s.ref(var, seg.x).get() + ", " + ...
+                         "[x, y, z]: [" + seg_x3d + ", " + seg_y3d + ", " + seg_z3d + "]");
                 end
 
             end
