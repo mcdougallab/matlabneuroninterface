00a5c86b6c851203685a7063db094db7dac8112c
edovanveen <edo.vanveen@vortech.nl>
2023-04-21 11:45:32 +0200

    Vector is subclass of Object; does not need separate isa

--- +neuron/hoc_push.m
+++ +neuron/hoc_push.m
@@ -1,15 +1,15 @@
 function hoc_push(value)
 % Push a double, string, Vector, Object or NrnRef to the NEURON stack.
 %   hoc_push(value)
-    if (isa(value, "double"))
+    if isa(value, "double")
         clib.neuron.hoc_pushx(value);  
-    elseif (isa(value, "string") || isa(value, "char"))
+    elseif isa(value, "string") || isa(value, "char")
         clib.neuron.matlab_hoc_pushstr(value);  
-    elseif (isa(value, "neuron.Vector") || isa(value, "neuron.Object"))
+    elseif isa(value, "neuron.Object")
         clib.neuron.hoc_push_object(value.obj);  
-    elseif (isa(value, "clib.neuron.Vector") || isa(value, "clib.neuron.Object"))
+    elseif isa(value, "clib.neuron.Object")
         clib.neuron.hoc_push_object(value);  
-    elseif (isa(value, "clib.neuron.NrnRef"))
+    elseif isa(value, "clib.neuron.NrnRef")
         clib.neuron.matlab_hoc_pushpx(value);
     else
         error("Input of type "+class(value)+" not allowed.");
