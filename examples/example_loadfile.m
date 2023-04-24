963db1d2eef901d116a182e5f017eb713a52a4f7
edovanveen <edo.vanveen@vortech.nl>
2023-04-24 10:34:07 +0200

    Use full path in load_file example

--- examples/example_loadfile.m
+++ examples/example_loadfile.m
@@ -5,6 +5,8 @@ n = neuron.Neuron();
 
 % Load stdrun.hoc; run n.continuerun
 n.t = 0;
-n.load_file('stdrun.hoc');
+examples_path = fileparts(mfilename('fullpath'));
+hoc_file_path = strrep(fullfile(examples_path, 'stdrun.hoc'), '\', '/');  % Path with backslashes does not work.
+n.load_file(hoc_file_path);
 n.continuerun(5);  % Only available if file loaded successfuly.
 disp(n.t);
