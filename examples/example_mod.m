6c6ae25a7ce8a311c1d11d59226f1b8efaf1eb54
edovanveen <edo.vanveen@vortech.nl>
2023-04-24 20:12:42 +0200

    Use OS independent movefile

--- examples/example_mod.m
+++ examples/example_mod.m
@@ -4,8 +4,8 @@ setup;
 n = neuron.Neuron();
 
 % Try to find 'hd' mechanism.
-axon1 = n.Section("axon1");
-axon1.insert_mechanism("hd");  % Warning: Insertable mechanism 'hd' not found. 
+% axon1 = n.Section("axon1");
+% axon1.insert_mechanism("hd");  % Error: Insertable mechanism 'hd' not found. 
 
 % Compile mod file.
 examples_path = fileparts(mfilename('fullpath'));
@@ -13,11 +13,11 @@ mod_path = fullfile(examples_path, 'mod');
 system("nrnivmodl " + mod_path);
 output_path = fullfile(pwd, 'nrnmech.dll');
 dll_path = fullfile(mod_path, 'nrnmech.dll');
-system("move " + output_path + " " + dll_path);
+movefile(output_path, dll_path);
 
 % Import dll into neuron.
 n.nrn_load_dll(strrep(dll_path, "\", "/"));
 
 % Try again to find 'hd' mechanism; now it should exist.
-axon2 = n.Section("axon2");
-axon2.insert_mechanism("hd");  % No warning.
+axon = n.Section("axon2");
+axon.insert_mechanism("hd");  % No warning.
