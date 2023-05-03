acb6aee79ad67a8faef8b6008cc2e134c628fa52
edovanveen <edo.vanveen@vortech.nl>
2023-05-03 12:05:40 +0200

    Added comments in examples.

--- examples/example_clamps.m
+++ examples/example_clamps.m
@@ -1,8 +1,15 @@
-% Initialization.
+% Minimal IClamp/SEClamp/VClamp/secondorder example.
+% adapted from https://nrn.readthedocs.io/en/8.2.2/python/modelspec/programmatic/mechanisms/mech.html#SEClamp
+% conceptually: this simulates an action potential, records it,
+% then feeds that in to various types of voltage clamps and sees
+% how much extra current they need to stabilize and how close the
+% voltage gets
+
 clear;
 setup;
 n = neuron.Neuron();
 
+% setup for three simulations
 s1 = n.Section("s1");
 s2 = n.Section("s2");
 s3 = n.Section("s3");
@@ -25,6 +32,7 @@ c3.dur(1) = 1;
 c3.dur(3) = 17;
 c3.amp = [0, 0, 0];
 
+% record an action potential
 ap = n.Vector();
 ap.record(s1(0.5).ref("v"));
 n.finitialize(-65);
@@ -32,7 +40,8 @@ while n.t < 1
     n.fadvance();
 end
 
-apc = ap.c(); 
+% do the three cases while playing the recorder ap
+apc = ap.c();  % unfortunately, cannot play into two variables, so clone it
 
 ap.play_remove();
 ap.play(c2.ref("amp1"), n.dt);
@@ -47,11 +56,14 @@ disp(c3.i);  % 6.0899e-06
 disp(s2(0.5).v);  % -65.0000
 disp(s3(0.5).v);  % -64.9987
 
+% using second order will give somewhat different results
+% and is in particular not recommended for the two electrode
+% voltage clamp VClamp as that system is numerically stiff
 n.secondorder = 2;
 
 n.finitialize(-65);
 n.fadvance();
 disp(c2.i);  % -1.7144e-05
-disp(c3.i);  % -65.0000
+disp(c3.i);  % -65.0000, NOTE: very different from first order (stiffness issue)
 disp(s2(0.5).v);  % -65.0000
 disp(s3(0.5).v);  % -64.9975
