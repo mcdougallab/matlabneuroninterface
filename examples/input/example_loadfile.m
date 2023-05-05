7d7d165f19716551ba4e883c54419d15b81cea1b
edovanveen <edo.vanveen@vortech.nl>
2023-05-05 14:59:10 +0200

    Small fixes on examples

--- examples/input/example_loadfile.m
+++ examples/input/example_loadfile.m
@@ -1,10 +1,13 @@
 % Initialization.
 n = neuron.Neuron();
+n.reset_sections();
+soma = n.Section("soma");
 
 % Load stdrun.hoc; run n.continuerun
-n.t = 0;
 examples_path = fileparts(mfilename('fullpath'));
 hoc_file_path = strrep(fullfile(examples_path, 'stdrun.hoc'), '\', '/');  % Path with backslashes does not work.
 n.load_file(hoc_file_path);
+
+n.finitialize(-65);
 n.continuerun(5);  % Only available if file loaded successfuly.
 disp(n.t);
