70ad31e211723ec4f1b56760331a69fe3e12ec54
edovanveen <edo.vanveen@vortech.nl>
2023-05-05 14:06:32 +0200

    Fix typo in assert, v->v2

--- +tests/test_base_functionality.m
+++ +tests/test_base_functionality.m
@@ -22,7 +22,7 @@ classdef test_base_functionality < matlab.unittest.TestCase
             assert(abs(v(3) - 42) < self.tol);
             assert(abs(v(11) - 5) < self.tol);
             % Asserts for second vector.
-            assert(isa(v, "neuron.Vector"));
+            assert(isa(v2, "neuron.Vector"));
             assert(abs(length(v2) - 6) < self.tol);
             assert(abs(v2(2) - 1) < self.tol);
             assert(abs(v2(6) - 5.3) < self.tol);
