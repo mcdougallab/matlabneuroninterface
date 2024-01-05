d0ff30a16f28c038089ca384dda5f4a4aa06b013
edovanveen <edo.vanveen@vortech.nl>
2024-01-05 12:21:24 +0100

    Warning on re-launch; hide Session constructor from user.

--- +neuron/launch.m
+++ +neuron/launch.m
@@ -1,5 +1,9 @@
 function n = launch()
 % Start a Neuron session.
 %   n = neuron.launch()
+    if clib.neuron.isinitialized()
+        warning("Neuron is already initialized: returned " + ...
+            "neuron.Session handle refers to the same object as before.");
+    end
     n = neuron.Session();
 end
