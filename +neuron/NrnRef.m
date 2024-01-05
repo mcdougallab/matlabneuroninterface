826a62e21b27b5380074cd7171d9d0be0b89e5e7
edovanveen <edo.vanveen@vortech.nl>
2024-01-05 15:23:25 +0100

    Add bounds check and documentation.

--- +neuron/NrnRef.m
+++ +neuron/NrnRef.m
@@ -1,5 +1,6 @@
 classdef NrnRef < handle
-% Neuron reference (NrnRef) wrapper class
+% Neuron reference (NrnRef) wrapper class. The referenced value can be
+% accessed by indexing, e.g. ```t = n.ref('t'); disp(t(1));```.
 
     properties (SetAccess=protected, GetAccess=public)
         obj             % C++ NrnRef object.
@@ -12,6 +13,9 @@ classdef NrnRef < handle
             self.obj = obj;
         end
         function value = get(self, ind)
+        % Get value.
+        %   value = get()
+        %   value = get(index)
             if exist('ind', 'var')
                 value = self.obj.get_index(ind - 1);
             else
@@ -19,18 +23,24 @@ classdef NrnRef < handle
             end
         end
         function self = set(self, value, ind)
+        % Set value.
+        %   value = set()
+        %   value = set(index)
             if exist('ind', 'var')
                 self.obj.set_index(value, ind - 1);
             else
                 self.obj.set(value);
             end
         end
+        function value = length(self)
+            value = self.obj.n_elements;
+        end
         % Allow for access by index.
         function varargout = subsref(self, S)
             if S(1).type == "()"
                 [varargout{1:nargout}] = self.get(S(1).subs{:});
                 n_processed = 1;
-            else
+            elseif S(1).type == "."
                 [varargout{1:nargout}] = builtin('subsref', self, S(1:2));
                 n_processed = 2;
             end
@@ -42,7 +52,7 @@ classdef NrnRef < handle
         function self = subsasgn(self, S, varargin)
             if S(1).type == "()"
                 self.set(varargin{:}, S(1).subs{:});
-            else
+            elseif S(1).type == "."
                 self = builtin('subsasgn', self, S, varargin{:});
             end
         end
