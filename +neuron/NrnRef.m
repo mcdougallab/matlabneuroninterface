ddab938a425a4a79e35698a4e92ed56298b996b3
edovanveen <edo.vanveen@vortech.nl>
2024-01-05 14:51:22 +0100

    NrnRef with indexing - no bounds checks!

--- +neuron/NrnRef.m
+++ +neuron/NrnRef.m
@@ -11,26 +11,39 @@ classdef NrnRef < handle
         %   NrnRef(obj) constructs a Matlab wrapper for C++ NrnRef obj
             self.obj = obj;
         end
-        function value = get(self)
-            value = self.obj.get();
-        end
-        function set(self, value)
-            self.obj.set(value);
-        end
-        % Allow for access by index (1).
-        function varargout = subsref(self, s)
-            if s(1).type == '()'  % TODO: check if index is correct?
-                [varargout{1:nargout}] = self.get();
+        function value = get(self, ind)
+            if exist('ind', 'var')
+                value = self.obj.get_index(ind - 1);
             else
-                [varargout{1:nargout}] = builtin('subsref', self, s);
+                value = self.obj.get();
             end
         end
-        function self = subsasgn(self, s, varargin)
-            disp(s);
-            if s(1).type == '()'
-                self.set(varargin{:});
+        function self = set(self, value, ind)
+            if exist('ind', 'var')
+                self.obj.set_index(value, ind - 1);
             else
-                self = builtin('subsasgn', self, s, varargin{:});
+                self.obj.set(value);
+            end
+        end
+        % Allow for access by index.
+        function varargout = subsref(self, S)
+            if S(1).type == "()"
+                [varargout{1:nargout}] = self.get(S(1).subs{:});
+                n_processed = 1;
+            else
+                [varargout{1:nargout}] = builtin('subsref', self, S(1:2));
+                n_processed = 2;
+            end
+            if numel(S) > n_processed
+                % Deal with a method/attribute call chain.
+                [varargout{1:nargout}] = varargout{:}.subsref(S(n_processed+1:end));
+            end
+        end
+        function self = subsasgn(self, S, varargin)
+            if S(1).type == "()"
+                self.set(varargin{:}, S(1).subs{:});
+            else
+                self = builtin('subsasgn', self, S, varargin{:});
             end
         end
     end
